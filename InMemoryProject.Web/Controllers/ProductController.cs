9cac5410209a9603213d693eae7637f25ee970c1
burakhayirli <burakhayirli1@hotmail.com>
2022-02-10 11:44:01 +0300

    Set first simple string cache and get

---
+++ InMemoryProject.Web/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InMemoryProject.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("time",DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.time= _memoryCache.Get<string>("time");
+            return View();
+        }
+    }
+}
