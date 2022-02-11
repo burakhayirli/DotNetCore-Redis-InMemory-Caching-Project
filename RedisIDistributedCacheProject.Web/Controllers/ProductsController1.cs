d1fc929b583b05476042b5445766af5e4846edb4
burakhayirli <burakhayirli1@hotmail.com>
2022-02-11 09:51:45 +0300

    Install Microsoft.Extensions.Caching.StackExchangeRedis package and use DI into controller

---
+++ RedisIDistributedCacheProject.Web/Controllers/ProductsController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisIDistributedCacheProject.Web.Controllers
+{
+    public class ProductsController1 : Controller
+    {
+        private IDistributedCache _distributedCache;
+        public ProductsController1(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
