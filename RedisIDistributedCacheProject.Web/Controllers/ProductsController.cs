39a9caa0ac3d01890ff831acfc0f6143a734c112
burakhayirli <burakhayirli1@hotmail.com>
2022-02-11 10:00:30 +0300

    Use SetString for adding value to Redis

--- RedisIDistributedCacheProject.Web/Controllers/ProductsController.cs
+++ RedisIDistributedCacheProject.Web/Controllers/ProductsController.cs
@@ -7,16 +7,22 @@ using System.Threading.Tasks;
 
 namespace RedisIDistributedCacheProject.Web.Controllers
 {
-    public class ProductsController1 : Controller
+    public class ProductsController : Controller
     {
         private IDistributedCache _distributedCache;
-        public ProductsController1(IDistributedCache distributedCache)
+        public ProductsController(IDistributedCache distributedCache)
         {
             _distributedCache = distributedCache;
         }
 
         public IActionResult Index()
         {
+            DistributedCacheEntryOptions options = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(1)
+            };
+
+            _distributedCache.SetString("name", "Burak",options);
             return View();
         }
     }
