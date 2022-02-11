1b36282f749ba3f28ecf23a4838f4b3a2317146e
burakhayirli <burakhayirli1@hotmail.com>
2022-02-11 10:27:51 +0300

    Use Complex Types as Json and Binary Serialize

--- RedisIDistributedCacheProject.Web/Controllers/ProductsController.cs
+++ RedisIDistributedCacheProject.Web/Controllers/ProductsController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using RedisIDistributedCacheProject.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace RedisIDistributedCacheProject.Web.Controllers
@@ -22,17 +25,45 @@ namespace RedisIDistributedCacheProject.Web.Controllers
                 AbsoluteExpiration = DateTime.Now.AddMinutes(1)
             };
 
-            _distributedCache.SetString("name", "Burak",options);
-            await _distributedCache.SetStringAsync("surname", "Hayırlı",options);
+            //_distributedCache.SetString("name", "Burak",options);
+            //await _distributedCache.SetStringAsync("surname", "Hayırlı",options);
+
+
+            //Complex Types With Json Serialize
+            Product product = new Product { Id = 1, Name = "Book", Price = 50 };
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            Product product2 = new Product { Id = 2, Name = "Book", Price = 50 };
+            string jsonProduct2 = JsonConvert.SerializeObject(product2);
+
+            //await _distributedCache.SetStringAsync("product:1", jsonProduct, options);
+            //await _distributedCache.SetStringAsync("product:2", jsonProduct2, options);
+
+            //Binary Format
+            Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+            _distributedCache.Set("product:1", byteProduct);
+
             return View();
         }
 
         public async Task<IActionResult> Show()
         {
-            string name = _distributedCache.GetString("name");
-            string surname = await _distributedCache.GetStringAsync("surname");
-            ViewBag.name = name;
-            ViewBag.surname = surname;
+            //string name = _distributedCache.GetString("name");
+            //string surname = await _distributedCache.GetStringAsync("surname");
+            //ViewBag.name = name;
+            //ViewBag.surname = surname;
+
+            //Complex Types Get Json
+            //string jsonProduct = await _distributedCache.GetStringAsync("product:1");
+            //Product product = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            //ViewBag.product = product;
+
+            //Binary Format Deserialize
+            Byte[] byteProduct = _distributedCache.Get("product:1");
+            string jsonProduct = Encoding.UTF8.GetString(byteProduct);
+            Product product = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            ViewBag.product = product;
+
             return View();
         }
 
