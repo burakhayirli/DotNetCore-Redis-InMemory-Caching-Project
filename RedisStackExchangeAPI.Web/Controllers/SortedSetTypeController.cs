77b0fe121d232dd32239590cc297a415f3391687
burakhayirli <burakhayirli1@hotmail.com>
2022-02-15 15:47:44 +0300

    Create SortedSetTypeController Use SotedSet methods

---
+++ RedisStackExchangeAPI.Web/Controllers/SortedSetTypeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisStackExchangeAPI.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisStackExchangeAPI.Web.Controllers
+{
+    public class SortedSetTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        private string setKey = "sortedset";
+        public SortedSetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(3);
+        }
+        public IActionResult Index()
+        {
+            HashSet<string> list = new HashSet<string>();
+            if (db.KeyExists(setKey))
+            {
+                //Default ASC
+                db.SortedSetScan(setKey).ToList().ForEach(x =>
+                {
+                    //x.Element
+                    //x.Score
+                    list.Add(x.Element.ToString());
+                });
+
+                //DESC
+                db.SortedSetRangeByRank(setKey, 0, 5, order: Order.Descending).ToList().ForEach(x =>
+                {
+                    list.Add(x.ToString());
+                });
+            }
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, int score)
+        {
+            db.SortedSetAdd(setKey, name, score);
+            db.KeyExpire(setKey, DateTime.Now.AddMinutes(2));
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteItem(string name)
+        {
+            await db.SortedSetRemoveAsync(setKey, name);
+            return RedirectToAction("Index");
+        }
+    }
+}
